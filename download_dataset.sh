ad67a2a07fea2e97e47947d59f297e7eb1d2bd33
Mathias Lechner <mathias.lechner@hotmail.com>
2023-04-27 11:00:18 -0400

    Updated walker download URL to MIT webspace

--- download_dataset.sh
+++ download_dataset.sh
@@ -6,6 +6,6 @@ mkdir data/person
 wget --no-check-certificate https://archive.ics.uci.edu/ml/machine-learning-databases/00196/ConfLongDemo_JSI.txt
 mv ConfLongDemo_JSI.txt data/person/
 
-wget https://pub.ist.ac.at/~mlechner/datasets/walker.zip
+wget https://www.mit.edu/~mlechner/walker.zip
 unzip walker.zip -d data/
 rm walker.zip
